f5c5e81ca4b164e54e56eb0691e5e16dc34b9f30
Elyon74 <antho74140@outlook.fr>
2021-10-27 01:21:11 +0200

    1.01
    - Base Game for dev open

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int CurrentPlayerHP = 4;
+    public int CurrentNumberOfCardInDeck = 5;
+    public int MaxNumberOfCardInDeck = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void HP()
+    {
+        if (CurrentPlayerHP == 3)
+        {
+
+        }
+        if (CurrentPlayerHP == 2)
+        {
+
+        }
+        if (CurrentPlayerHP == 1)
+        {
+
+        }
+        if (CurrentPlayerHP == 0)
+        {
+
+        }
+    }
+    public void CardNumber()
+    {
+        if (CurrentNumberOfCardInDeck == 4)
+        {
+
+        }
+        if (CurrentNumberOfCardInDeck == 4)
+        {
+
+        }
+        if (CurrentNumberOfCardInDeck == 4)
+        {
+
+        }
+        if (CurrentNumberOfCardInDeck == 4)
+        {
+
+        }
+        if (CurrentNumberOfCardInDeck == 4)
+        {
+
+        }
+        if (MaxNumberOfCardInDeck > 5)
+        {
+
+        }
+    }
+}
